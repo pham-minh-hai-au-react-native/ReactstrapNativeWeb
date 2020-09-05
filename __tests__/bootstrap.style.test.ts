98f082c7f3e7f992089d94aacbaa05e44e6ec1c7
pham minh hai au <phamau1994x@gmail.com>
2020-09-05 17:31:21 +0700

    feat: Handle build dynamic theme
    Handle build dynamic theme

--- __tests__/bootstrap.style.test.ts
+++ __tests__/bootstrap.style.test.ts
@@ -1,7 +1,48 @@
 import {buildTheme} from '../src/styles';
+import objectContaining = jasmine.objectContaining;
 
-describe('Write test case demo', () => {
-  it('Write first test case', () => {
-    const result = buildTheme({});
+describe('Write test for function buildTheme', () => {
+  it('Write test for case input is undefined', () => {
+    const result = buildTheme();
+    expect(result).toEqual(
+      objectContaining({
+        default: {
+          colors: {primary: '#007bff'},
+          textColor: {
+            textPrimary: {
+              color: '#007bff',
+            },
+          },
+        },
+      }),
+    );
   });
+  it('Write test for case input is only contain default theme', async () => {
+    const result = buildTheme({
+      default: {
+        colors: {
+          secondary: 'red',
+        },
+      },
+    });
+    expect(result).toEqual(
+      objectContaining({
+        default: {
+          colors: {
+            primary: '#007bff',
+            secondary: 'red',
+          },
+          textColor: {
+            textPrimary: {
+              color: '#007bff',
+            },
+            textSecondary: {
+              color: 'red',
+            },
+          },
+        },
+      }),
+    );
+  });
+  it('Write test for case input is contain multiple theme', async () => {});
 });
--- src/styles/bootstrap.style.ts
+++ src/styles/bootstrap.style.ts
@@ -4,41 +4,67 @@ import {ColorValue, StyleSheet, TextStyle} from 'react-native';
 const upperFirstLetter = (str: string): string => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
+
 interface VariableColor {
   [key: string]: ColorValue;
 }
+
 interface VariableTextColor {
   [key: string]: Required<Pick<TextStyle, 'color'>>;
 }
+
 interface Color {
   colors: VariableColor;
 }
+
 interface TextColor {
   textColor: VariableTextColor;
 }
+
 interface Theme extends Color, TextColor {}
+
 interface DynamicTheme extends Color {}
+
 interface ListTheme {
   [key: string]: Theme;
 }
+
 interface ListDynamicTheme {
   [key: string]: DynamicTheme;
 }
+
 const theme: DynamicTheme = {
   colors: {
     primary: '#007bff',
   },
 };
-export const buildTheme = (listTheme?: ListDynamicTheme): any => {
-  const getColors = listTheme?.hasOwnProperty('default')
-    ? listTheme?.default.colors
-    : {};
-  const {colors} = theme;
-  console.log('in ra getColors');
-  console.log(getColors);
-  console.log('in ra colors');
-  console.log(colors);
-  console.log('demo');
+export const buildTheme = (listTheme?: ListDynamicTheme): ListTheme => {
+  const result: ListTheme = {};
+  if (listTheme === undefined) {
+    result.default = {colors: {}, textColor: {}};
+    result.default.colors = theme.colors;
+    for (const propertyColor in theme.colors) {
+      result.default.textColor[`text${upperFirstLetter(propertyColor)}`] = {
+        color: theme.colors[propertyColor],
+      };
+    }
+    return result;
+  }
+  listTheme.default.colors = listTheme.hasOwnProperty('default')
+    ? {...listTheme.default.colors, ...theme.colors}
+    : theme.colors;
+
+  for (const propertyTheme in listTheme) {
+    result[propertyTheme] = {...listTheme[propertyTheme], ...{textColor: {}}};
+    for (const propertyColor in listTheme[propertyTheme].colors) {
+      result[propertyTheme].textColor[
+        `text${upperFirstLetter(propertyColor)}`
+      ] = {
+        color: listTheme[propertyTheme].colors[propertyColor],
+      };
+    }
+  }
+  return result;
 };
 export const layout = StyleSheet.create(Layout);
 
