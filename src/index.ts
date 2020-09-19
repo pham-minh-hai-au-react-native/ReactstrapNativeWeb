e6262bef77656796fbbf5d9569c6e4eaeee11f1b
pham minh hai au <phamau1994x@gmail.com>
2020-09-19 20:44:16 +0700

    test: test pipeline 3.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {Container, Image, Row, Col, TextShadow} from './components/index';
 export * from './styles/index';
+const a = 10;
 console.log('test');
 export {Container, Row, Col, Image, TextShadow};
