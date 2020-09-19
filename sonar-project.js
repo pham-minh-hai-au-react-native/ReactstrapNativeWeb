6526524279fa057c98fffc9f24b77c9446c3a077
pham minh hai au <phamau1994x@gmail.com>
2020-09-19 16:37:59 +0700

    test: test pipeline.

--- sonar-project.js
+++ sonar-project.js
@@ -1,17 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
+
 sonarqubeScanner(
   {
-    serverUrl: 'http://localhost:9000',
-    token: 'b0a08314c28ad6f4cdbcb6b123fb4e787946783c',
-    options: {
-      'sonar.projectName': 'ReactstrapNativeWeb',
-      'sonar.sources': 'src',
-      'sonar.exclusions': '**/__tests__/**',
-      'sonar.tests': '',
-      'sonar.test.inclusions': '',
-      'sonar.typescript.lcov.reportPaths': '',
-      'sonar.testExecutionReportPaths': '',
-    },
+    serverUrl: process.env.SONAR_SERVER || 'http://localhost:9000',
+    token:
+      process.env.SONAR_TOKEN || '96d99b449e01aab64e2c35298c5bd2b239595a03',
+    options: {},
   },
-  () => {},
+  () => {
+    console.log('>> Sonar analysis is done!');
+  },
 );
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import {Container, Image, Row, Col, TextShadow} from './components/index';
-
 export * from './styles/index';
 export {Container, Row, Col, Image, TextShadow};
